a91eba605c3c3430e4d69aa3fd680233c64295fb
Artem Golotin <agolotin@Artems-MBP.home>
2016-01-30 19:03:54 -0700

    Kafka code that does not work...

---
+++ src/kafka_messaging/ProducerKafka.java
@@ -0,0 +1,89 @@
+package kafka_messaging;
+
+//import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Properties;
+import java.util.Random;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import kafka.javaapi.producer.Producer;
+//import org.apache.kafka.clients.producer.KafkaProducer;
+//import org.apache.kafka.clients.producer.ProducerRecord;
+//import org.apache.kafka.clients.producer.Partitioner;
+
+
+import kafka.producer.KeyedMessage;
+//import kafka.producer.ProducerConfig;
+import kafka.common.TopicExistsException;
+
+
+@SuppressWarnings({ "rawtypes", "unchecked" })
+public class ProducerKafka {
+	
+//	private ProducerConfig config;
+	private Properties props;
+	private Producer producer;
+	
+	public ProducerKafka() {}
+	
+	public static void main(String[] args) {
+		
+		ProducerKafka prod = new ProducerKafka();
+		try {
+			prod.defineKafkaProducer();
+			prod.runKafkaProducer();
+		}	
+		catch (TopicExistsException | IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	
+	public void defineKafkaProducer() throws IOException {
+		// Define producer properties
+		this.props = new Properties();
+		props.put("serializer.class", "kafka.serializer.DefaultEncoder");//"org.bigdata.kafka.Serializer");
+        props.put("key.serializer.class", "kafka.serializer.StringEncoder");
+        props.put("metadata.broker.list", "127.0.0.1:9092");//"192.168.1.104:9092");
+//		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.1.104:9092");
+//		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+//		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+////        props.load(new FileInputStream("producer.properties"));
+//		props.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, SimplePartitioner.class.getName());
+		 
+		// Define actual producer
+		// first String is partition key, and the second is the type of the message
+		this.producer = new Producer(new kafka.producer.ProducerConfig(this.props));
+	}
+	
+	public void runKafkaProducer() {
+    	System.out.println("Kafka Producer starting...");
+    	// log4j writes to stdout for now
+		org.apache.log4j.BasicConfigurator.configure();
+		
+		try {
+			this.defineKafkaProducer();
+		} catch (IOException e) {
+			System.out.println("Could not define Kafka Producer settings");
+			e.printStackTrace();
+		}
+		
+		// This is a fake message
+		// event_data is a topic, IP is a partition key, 
+		KeyedMessage<String, byte[]> data = new KeyedMessage("test", "test-message".getBytes(StandardCharsets.UTF_8));
+		
+		ArrayList<KeyedMessage> messages = new ArrayList<KeyedMessage>();
+        messages.add(data);
+        
+        this.producer.send(messages);
+        this.producer.close();
+	}
+
+}
---
+++ src/kafka_server/ServerKafka.java
@@ -0,0 +1,128 @@
+package kafka_server;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Properties;
+
+import kafka.utils.ZkUtils;
+
+import org.I0Itec.zkclient.ZkClient;
+import org.apache.zookeeper.server.ServerConfig;
+import org.apache.zookeeper.server.ZooKeeperServerMain;
+import org.apache.zookeeper.server.quorum.QuorumPeerConfig;
+
+import kafka.utils.ZKStringSerializer$;
+import kafka.admin.AdminUtils;
+import kafka.server.KafkaConfig;
+import kafka.server.KafkaServerStartable;
+ 
+ 
+@SuppressWarnings("unused")
+public class ServerKafka {
+	
+ 
+	private static KafkaServerStartable kafka;
+	private static ZooKeeperLocal zookeeper;
+	
+	
+	public ServerKafka(Properties kafkaProperties, Properties zkProperties) throws IOException, InterruptedException{
+		setUpZookeeper(zkProperties);
+		setUpKafkaServer(kafkaProperties);
+	}
+	
+	public static void setUpZookeeper(Properties zkProp) throws FileNotFoundException, IOException {
+		// Start local Zookeeper
+		System.out.println("Starting local zookeeper...");
+		zookeeper = new ZooKeeperLocal(zkProp);
+		System.out.println("Done");
+	}
+	
+	public static void setUpKafkaServer(Properties kafkaProp) {
+		KafkaConfig kafkaConfig = new KafkaConfig(kafkaProp);
+		// Start local kafka broker
+		kafka = new KafkaServerStartable(kafkaConfig);
+		System.out.println("Starting local kafka broker...");
+		kafka.startup();
+		System.out.println("Done");
+	}
+	
+	
+	public void stop() {
+		// Stop kafka broker
+		System.out.println("Stopping kafka...");
+		kafka.shutdown();
+		System.out.println("Done");
+
+		/*
+		System.out.println("Stopping zookeeper...");
+		zookeeper.stop();
+		System.out.println("Done");
+		*/
+		
+		kafka = null;
+		zookeeper = null;
+	}
+	
+	public static class ZooKeeperLocal {
+		
+		private ZooKeeperServerMain zkServer;
+		//private Thread zkRunnable;
+		
+		// Class instance is invoked in kafka_server.KafkaServerLocal
+		public ZooKeeperLocal(Properties zkProperties) throws FileNotFoundException, IOException{
+			QuorumPeerConfig quorumConfiguration = new QuorumPeerConfig();
+			try {
+				quorumConfiguration.parseProperties(zkProperties);
+			} catch(Exception e) {
+				throw new RuntimeException(e);
+			}
+	 
+			zkServer = new ZooKeeperServerMain();
+			final ServerConfig configuration = new ServerConfig();
+			configuration.readFrom(quorumConfiguration);
+			
+			new Thread() {
+				public void run() {
+					try {
+						zkServer.runFromConfig(configuration);
+					} catch (IOException e) {
+						System.out.println("ZooKeeper start failed");
+						e.printStackTrace(System.err);
+					}
+				}
+			   
+			}.start();
+		}
+		
+		/*
+		public void stop() {
+			try {
+				Method shutdown = ZooKeeperServerMain.class.getDeclaredMethod("shutdown");
+				shutdown.setAccessible(true);
+				shutdown.invoke(zkRunnable);
+				System.out.println("ZooKeeper server stopped successfully");
+				
+			} catch (Exception e) {
+				System.out.println("ZooKeeper stop failed");
+				e.printStackTrace(System.err);
+			}
+		}
+
+		public void createZKTopic(String topicName) {
+			// Create a ZooKeeper client
+			int sessionTimeoutMs = 10000;
+			int connectionTimeoutMs = 10000;
+			
+			ZkClient zkClient = new ZkClient("127.0.0.1:2181", sessionTimeoutMs, connectionTimeoutMs, ZKStringSerializer$.MODULE$);
+			ZkUtils zkUtils = new ZkUtils(zkClient, null, false);
+			
+			// Create a topic named "whatever_name" with 1 partition and a replication factor of 1
+			int numPartitions = 1;
+			int replicationFactor = 1;
+			AdminUtils.createTopic(zkUtils, topicName, numPartitions, replicationFactor, new Properties());
+		}
+		*/
+	}
+}
