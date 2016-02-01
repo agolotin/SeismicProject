2b604509c07d08fab007eed55df4a046dc3cbf4e
Steven Magaña-Zook <maganazook1@llnl.gov>
2016-02-01 15:26:25 -0800

    Converted this project into a Maven project for automatic dependency management, and easier configuration. Pom.xml specifies dependency on IRIS, Ignite, and Kafka with transitive dependencies downloaded automatically.

---
+++ src/main/java/edu/byu/kafka_messaging/IrisTest.java
@@ -0,0 +1,90 @@
+package edu.byu.kafka_messaging;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.text.*;
+import java.util.*;
+
+import edu.iris.dmc.criteria.*;  
+import edu.iris.dmc.service.*;
+import edu.iris.dmc.timeseries.model.Segment;
+import edu.iris.dmc.timeseries.model.Timeseries;
+
+public class IrisTest {
+
+	public static void main(String[] args) {
+		IrisTest tester = new IrisTest();
+		tester.run();
+	
+	}
+	
+	private void run() {
+		ServiceUtil serviceUtil = ServiceUtil.getInstance();
+		serviceUtil.setAppName("Tutorials");
+		WaveformService waveformService = serviceUtil.getWaveformService();
+		
+		
+		DateFormat dfm = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+		dfm.setTimeZone(TimeZone.getTimeZone("GMT"));
+		Date startDate = null;
+		Date endDate = null;
+		try {
+			startDate = dfm.parse("2005-02-17T00:00:00");
+			endDate = dfm.parse("2005-02-31T00:00:00");
+//			endDate = dfm.parse("2015-12-31T00:00:00");
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		WaveformCriteria criteria = new WaveformCriteria();
+		criteria.add("IU", "KBS", "00", "BHZ", startDate, endDate);
+		
+		List<Timeseries> timeSeriesCollection = null;
+		try {
+			timeSeriesCollection = waveformService.fetch(criteria);
+		} catch (NoDataFoundException | IOException | CriteriaException | ServiceNotSupportedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
+		for(Timeseries timeseries:timeSeriesCollection){
+			   System.out.println(timeseries.getNetworkCode() + "-" +
+			   timeseries.getStationCode() + " (" + timeseries.getChannelCode() + "), loc:" +
+			      timeseries.getLocation());
+			      for(Segment segment:timeseries.getSegments()){
+			         System.out.printf("Segment:\n");
+			         System.out.printf("  Start: %s", segment.getStartTime());
+			         System.out.printf("  End: %s", segment.getEndTime());
+			         System.out.printf(":  %d samples exist in this segment\n",
+			            segment.getSampleCount());         
+			   }
+		}
+		
+	}
+
+	
+	public int checkSize() {
+		Serializable ser = null;
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		ObjectOutputStream oos = null;
+		
+		try {
+			oos = new ObjectOutputStream(baos);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			oos.writeObject(ser);
+			oos.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return baos.size();
+	}
+	
+
+}
