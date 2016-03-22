400426445f076199ca21fa05a35bef34449ceea8
Artem Golotin <artem.golotin@gmail.com>
2016-03-21 18:10:54 -0600

    Added a comment to partitioner class

--- src/main/java/edu/byu/seismicproject/producer/StreamPartitioner.java
+++ src/main/java/edu/byu/seismicproject/producer/StreamPartitioner.java
@@ -19,6 +19,14 @@ public class StreamPartitioner implements Partitioner {
 	public int partition(String topic, Object key, byte[] keyBytes, 
 			Object value, byte[] valueBytes, Cluster clusterMetadata) {
 		
+		/* Technically, such easy of a logic does not require us to have a separate
+		 * class for our partitioner. This class is implemented for our convenience 
+		 * later when we might decide to have less partitions than we have bands.
+		 * With this logic, however, the number of partitions should never exceed 
+		 * the number of bands we have, because in that case we will have partitions 
+		 * that do not have any data associated with them
+		 */
+		
 		int bandNum = ((StreamSegment) value).getId().getBand().getBandNum();
 		int numPartitions = clusterMetadata.partitionCountForTopic(topic);
 		
